a96824855492f225d0fb20c5ff0adab52afe4842
Ayaz-Nazar <75629355+AyazNazar-hub@users.noreply.github.com>
2021-01-24 09:31:33 +0500

    Add files via upload

---
+++ FireBase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const Firebase = firebase.initializeApp({
+  apiKey: 'AIzaSyDr_yTq4N8D1u_TK-y55gVg7asWwD4BC84',
+  authDomain: 'payingguest-d54ce.firebaseapp.com',
+  databaseURL: 'https://payingguest-d54ce.firebaseio.com',
+  projectId: 'payingguest-d54ce',
+  storageBucket: 'payingguest-d54ce.appspot.com',
+  messagingSenderId: '582114258956',
+  appId: '1:582114258956:web:c0bf16b3b46ff4be8fbe29',
+  measurementId: 'G-4K1WNDBSN9',
+});
+
+export default Firebase;
